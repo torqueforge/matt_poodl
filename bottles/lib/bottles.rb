efcbb4234f57f283137f7c0fb6976df2ebdba4b0
Sandi Metz <skmetz@gmail.com>
2014-06-12 17:04:53 -0400

    Use #quantity in both strings.

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -2,9 +2,9 @@ class Bottles
 
   def verse(number)
     if number >= 1
-      "#{quantity(number).capitalize} #{container(number)} of beer on the wall, #{number} #{container(number)} of beer.\n" + "Take #{pronoun(number)} down and pass it around, #{quantity(number-1)} #{container(number-1)} of beer on the wall.\n"
+      "#{quantity(number).capitalize} #{container(number)} of beer on the wall, #{quantity(number)} #{container(number)} of beer.\n" + "Take #{pronoun(number)} down and pass it around, #{quantity(number-1)} #{container(number-1)} of beer on the wall.\n"
     elsif number == 0
-      "#{quantity(number).capitalize} #{container(number)} of beer on the wall, no more bottles of beer.\n" + "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
+      "#{quantity(number).capitalize} #{container(number)} of beer on the wall, #{quantity(number)} bottles of beer.\n" + "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
     end
   end
 
