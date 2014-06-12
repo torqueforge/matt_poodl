33a3340f88a3bdd9bc7c556fb403abc4eb75894c
Sandi Metz <skmetz@gmail.com>
2014-06-12 12:55:27 -0400

    Make like things more alike
    Imagine that 99 Bottles was a production app instead of a fixed song.
    In this case we would likely not stop at Shameless Green.
    The most obvious problem with this code is the duplication
    of strings.  Removing string duplication lets us practice
    a very specific refactoring technique.
    
    This technique, in the micro, has three repeated steps, and
    in the macro, has a pattern of completing _horizontal_
    refactorings before getting distracted by _vertical_
    refactorings (the meaning of these terms will become
    clear) (I hope).
    
    The steps in the mico technique are
    1. compile new code
    2. execute new code
    3. use the result of new code.
    
    Sometimes steps 2 and 3 are combined, but step 1 always happens
    independently.
    
    The attack plan here is to pick the two strings that are the
    most alike, and to make them *more* alike by the smallest
    steps possible.
    
    The number > 2 case is almost just like the number > 1 case
    so I will start there.  If they sent a message to get
    bottle/bottles they would be just alike.
    
    The message will be #containers.  Adding a #containers method
    that returns _bottles_, the most common case, without
    actually sending the #containers method, is step1
    of the micro refactoring.

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -1,6 +1,25 @@
-# Feel free to delete the instructions once you get going
-puts instructions(__FILE__)
-
 class Bottles
+
+  def verse(number)
+    if number > 2
+      "#{number} bottles of beer on the wall, #{number} bottles of beer.\n" + "Take one down and pass it around, #{number-1} bottles of beer on the wall.\n"
+    elsif number > 1
+      "#{number} bottles of beer on the wall, #{number} bottles of beer.\n" + "Take one down and pass it around, #{number-1} bottle of beer on the wall.\n"
+    elsif number == 1
+      "#{number} bottle of beer on the wall, #{number} bottle of beer.\n" + "Take it down and pass it around, no more bottles of beer on the wall.\n"
+    elsif number == 0
+      %q{No more bottles of beer on the wall, no more bottles of beer.
+Go to the store and buy some more, 99 bottles of beer on the wall.
+}
+    end
+  end
+
+  def verses(first, last)
+    (last..first).map { |i| verse(i) }.reverse.join("\n")
+  end
+
+  def song
+    verses(99, 0)
+  end
 
 end
