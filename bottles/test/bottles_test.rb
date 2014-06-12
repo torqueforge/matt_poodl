5ed1ff1dc174339cbafc9d3492d8eebb4e9d0330
Sandi Metz <skmetz@gmail.com>
2014-06-12 12:39:37 -0400

    remove skips from test

--- bottles/test/bottles_test.rb
+++ bottles/test/bottles_test.rb
@@ -17,7 +17,6 @@ VERSE
   end
 
   def test_another_verse
-    skip
     expected = <<-VERSE
 89 bottles of beer on the wall, 89 bottles of beer.
 Take one down and pass it around, 88 bottles of beer on the wall.
@@ -26,7 +25,6 @@ VERSE
   end
 
   def test_verse_2
-    skip
     expected = <<-VERSE
 2 bottles of beer on the wall, 2 bottles of beer.
 Take one down and pass it around, 1 bottle of beer on the wall.
@@ -35,7 +33,6 @@ Take one down and pass it around, 1 bottle of beer on the wall.
   end
 
   def test_verse_1
-    skip
     expected = <<-VERSE
 1 bottle of beer on the wall, 1 bottle of beer.
 Take it down and pass it around, no more bottles of beer on the wall.
@@ -44,7 +41,6 @@ Take it down and pass it around, no more bottles of beer on the wall.
   end
 
   def test_verse_0
-    skip
     expected = <<-VERSE
 No more bottles of beer on the wall, no more bottles of beer.
 Go to the store and buy some more, 99 bottles of beer on the wall.
@@ -53,7 +49,6 @@ Go to the store and buy some more, 99 bottles of beer on the wall.
   end
 
   def test_a_couple_verses
-    skip
     expected = <<-VERSES
 99 bottles of beer on the wall, 99 bottles of beer.
 Take one down and pass it around, 98 bottles of beer on the wall.
@@ -65,7 +60,6 @@ VERSES
   end
 
   def test_a_few_verses
-    skip
     expected = <<-VERSES
 2 bottles of beer on the wall, 2 bottles of beer.
 Take one down and pass it around, 1 bottle of beer on the wall.
@@ -80,7 +74,6 @@ VERSES
   end
 
   def test_the_whole_song
-    skip
     assert_equal bottles.verses(99, 0), bottles.song
   end
 end
