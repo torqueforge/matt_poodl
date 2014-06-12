b411c8cdbdfcca2fadb1feeee0dc00aefa559554
Sandi Metz <skmetz@gmail.com>
2014-06-12 16:24:26 -0400

    Make > 1 more like == 0.
    The next two most alike strings are the > 0 and == 0 cases.
    I will make them identical going left to right.
    
    This first change uses #container for _bottles_ in the > 1 string.

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -2,7 +2,7 @@ class Bottles
 
   def verse(number)
     if number > 1
-      "#{number} bottles of beer on the wall, #{number} bottles of beer.\n" + "Take one down and pass it around, #{number-1} #{container(number-1)} of beer on the wall.\n"
+      "#{number} #{container(number)} of beer on the wall, #{number} bottles of beer.\n" + "Take one down and pass it around, #{number-1} #{container(number-1)} of beer on the wall.\n"
     elsif number == 1
       "#{number} bottle of beer on the wall, #{number} bottle of beer.\n" + "Take it down and pass it around, no more bottles of beer on the wall.\n"
     elsif number == 0
