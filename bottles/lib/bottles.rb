3270b2db828b24a7ca5b91639bdd2f8bd716d14c
Sandi Metz <skmetz@gmail.com>
2014-06-12 16:56:25 -0400

    Use #quantity in other string.

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -2,7 +2,7 @@ class Bottles
 
   def verse(number)
     if number > 1
-      "#{number} #{container(number)} of beer on the wall, #{number} #{container(number)} of beer.\n" + "Take #{pronoun(number)} down and pass it around, #{number-1} #{container(number-1)} of beer on the wall.\n"
+      "#{number} #{container(number)} of beer on the wall, #{number} #{container(number)} of beer.\n" + "Take #{pronoun(number)} down and pass it around, #{quantity(number-1)} #{container(number-1)} of beer on the wall.\n"
     elsif number == 1
       "#{number} #{container(number)} of beer on the wall, #{number} #{container(number)} of beer.\n" + "Take #{pronoun(number)} down and pass it around, #{quantity(number-1)} bottles of beer on the wall.\n"
     elsif number == 0
