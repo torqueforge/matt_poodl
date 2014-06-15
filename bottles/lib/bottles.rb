d466726090ae23111f8adf748759c1293df5a1af
Sandi Metz <skmetz@gmail.com>
2014-06-15 09:02:25 -0400

    Then remove the arguments from the first caller.

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -20,7 +20,7 @@ class Verse
   end
 
   def verse
-    "#{quantity(number).capitalize} #{initial_container} of beer on the wall, " +
+    "#{quantity.capitalize} #{initial_container} of beer on the wall, " +
     "#{quantity(number)} #{initial_container} of beer.\n" +
     "#{action}, " +
     "#{quantity(number-1)} #{final_container} of beer on the wall.\n"
