8235dd113d4ebadf45ac833db68bf15193785dd2
Sandi Metz <skmetz@gmail.com>
2014-06-15 07:39:00 -0400

    compile a simpler verses interator

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -5,6 +5,7 @@ class Bottles
   end
 
   def verses(upper_bound, lower_bound)
+    upper_bound.downto(lower_bound).map { |i| verse(i) }.join("\n")
     (lower_bound..upper_bound).map { |i| verse(i) }.reverse.join("\n")
   end
 
