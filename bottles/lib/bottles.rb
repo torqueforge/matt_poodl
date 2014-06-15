e228f7cfee115c8fa2e784ba2a484db6260ef2e9
Sandi Metz <skmetz@gmail.com>
2014-06-15 07:58:14 -0400

    Return to task: Add 6-packs.
    1. Is this open/closed to 6-packs?  No.
    2. Do I know how to make it open closed to 6-packs? No, so
    3. Fix the most egregious code smell.
    
    There are several code smells here, all pointing to the same problem.
    The most obvious smell is that many methods take the same argument,
    and in those methods, the code depends more on the argument than
    on the rest of the class.
    
    Look at the methods that take _number_.  This pattern means that
    _number_ represents a concept that should be a full-blown object,
    and that all of the methods that take _number_ should be implemented
    in that new object.
    
    This commit is the first step along the path of separating _number_ into
    another object.  We choose Verse as the name for the new object, and,
    using the Katrina refactoring techniques, begin by copying (not cutting),
    the methods into the new class.

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -51,3 +51,47 @@ class Bottles
   end
 
 end
+
+class Verse
+  def verse(number)
+    "#{quantity(number).capitalize} #{container(number)} of beer on the wall, " +
+    "#{quantity(number)} #{container(number)} of beer.\n" +
+    "#{action(number)}, " +
+    "#{quantity(number-1)} #{container(number-1)} of beer on the wall.\n"
+  end
+
+  private
+  def container(number)
+    if number == 1
+      'bottle'
+    else
+      'bottles'
+    end
+  end
+
+  def pronoun(number)
+    if number == 1
+      'it'
+    else
+      'one'
+    end
+  end
+
+  def quantity(number)
+    if number == -1
+      99.to_s
+    elsif number == 0
+      'no more'
+    else
+      number.to_s
+    end
+  end
+
+  def action(number)
+    if number == 0
+      "Go to the store and buy some more"
+    else
+      "Take #{pronoun(number)} down and pass it around"
+    end
+  end
+end
