b05ed729574200e615a5c942bfe89a1af8ce70e7
Sandi Metz <skmetz@gmail.com>
2014-06-15 12:44:10 -0400

    Remove argument from #pronoun.

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -68,7 +68,7 @@ class BottleNumber
     end
   end
 
-  def pronoun(number)
+  def pronoun
     if number == 1
       'it'
     else
@@ -90,7 +90,7 @@ class BottleNumber
     if number == 0
       "Go to the store and buy some more"
     else
-      "Take #{pronoun(number)} down and pass it around"
+      "Take #{pronoun} down and pass it around"
     end
   end
 end
