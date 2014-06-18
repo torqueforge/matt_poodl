83f20ccf0db7699e213add0096f860eddfe88c35
Sandi Metz <skmetz@gmail.com>
2014-06-18 09:04:58 -0400

    move files around

--- farm/test/farm_test.rb
+++ farm/test/farm_test.rb
@@ -23,6 +23,16 @@ class AnimalsTest < Minitest::Test
     animals = Animal.all(['pig', 'owl'])
     assert_equal 2, animals.size
   end
+
+  def test_missing_animal
+    animal = Animal.all(['']).first
+    assert_nil animal
+  end
+
+  def test_pig_and_missing_animal_and_owl
+    animals = Animal.all(['pig', '', 'owl'])
+    assert_equal 3, animals.size
+  end
 end
 
 
@@ -38,7 +48,6 @@ Old MacDonald had a farm, E-I-E-I-O.]
   end
 
   def test_pig_and_owl
-    skip
     expected =
     %q[Old MacDonald had a farm, E-I-E-I-O,
 And on that farm he had a pig, E-I-E-I-O,
@@ -52,5 +61,27 @@ With a hoot hoot here and a hoot hoot there,
 Here a hoot, there a hoot, everywhere a hoot hoot,
 Old MacDonald had a farm, E-I-E-I-O.]
     assert_equal expected, Farm.new(['pig', 'owl']).lyrics
+  end
+
+  def test_pig_and_nothing_and_duck
+    expected =
+    %q[Old MacDonald had a farm, E-I-E-I-O,
+And on that farm he had a pig, E-I-E-I-O,
+With an oink oink here and an oink oink there,
+Here an oink, there an oink, everywhere an oink oink,
+Old MacDonald had a farm, E-I-E-I-O.
+
+Old MacDonald had a farm, E-I-E-I-O,
+And on that farm he had a <silence>, E-I-E-I-O,
+With a <silence> <silence> here and a <silence> <silence> there,
+Here a <silence>, there a <silence>, everywhere a <silence> <silence>,
+Old MacDonald had a farm, E-I-E-I-O.
+
+Old MacDonald had a farm, E-I-E-I-O,
+And on that farm he had a duck, E-I-E-I-O,
+With a quack quack here and a quack quack there,
+Here a quack, there a quack, everywhere a quack quack,
+Old MacDonald had a farm, E-I-E-I-O.]
+    assert_equal expected, Farm.new(['pig', '', 'duck']).lyrics
   end
 end
